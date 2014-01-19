0242696ee43d91415d8ad55698b280b15cbc201b
Michael L Perry <michael@qedcode.com>
2014-01-19 00:03:44 -0600

    Mock out course page.

--- Principia/MainPage.xaml.cs
+++ Principia/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Principia.Courses.Views;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -25,6 +26,11 @@ namespace Principia
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void NewCourse_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(CoursePage));
         }
     }
 }
