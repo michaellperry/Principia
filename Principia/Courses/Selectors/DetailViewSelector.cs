525d4aa454742b3d25147a1f2218b159885a27b5
Michael L Perry <michael@qedcode.com>
2014-01-28 22:08:22 -0600

    Select the detail view based on data type.

---
+++ Principia/Courses/Selectors/DetailViewSelector.cs
@@ -0,0 +1,22 @@
+﻿using Principia.Courses.ViewModels;
+using UpdateControls.XAML;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Principia.Courses.Selectors
+{
+    public class DetailViewSelector : DataTemplateSelector
+    {
+        protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+        {
+            if (ForView.Unwrap<CourseDetailViewModel>(item) != null)
+                return Application.Current.Resources["CourseDataTemplate"] as DataTemplate;
+            if (ForView.Unwrap<ModuleDetailViewModel>(item) != null)
+                return Application.Current.Resources["ModuleDataTemplate"] as DataTemplate;
+            if (ForView.Unwrap<ClipDetailViewModel>(item) != null)
+                return Application.Current.Resources["ClipDataTemplate"] as DataTemplate;
+
+            return base.SelectTemplateCore(item, container);
+        }
+    }
+}
